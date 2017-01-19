14ac51353410fa9f8cc8b4244cc9affe63bdb8a4
Steve Küng <steve.kueng@srgssr.ch>
2017-01-19 16:01:21 +0100

    update

--- docker/start.sh
+++ docker/start.sh
@@ -2,9 +2,9 @@
 # /usr/local/bin/nfs-client
 sleep 2
 service nginx start
-/usr/sbin/in.tftpd -l --permissive /nbi --blocksize ${TFTP_BLOCKSIZE}
+/usr/sbin/in.tftpd -l --permissive /nbi -B ${TFTP_BLOCKSIZE}
 cd /bsdpy
 git pull
-./bsdpserver.py -p ${DOCKER_BSDPY_PATH} -i ${DOCKER_BSDPY_IFACE} -r ${DOCKER_BSDPY_PROTO} &
+./bsdpserver.py -i ${DOCKER_BSDPY_IFACE} -r ${DOCKER_BSDPY_PROTO} &
 sleep 2
 tail -f /var/log/bsdpserver.log
